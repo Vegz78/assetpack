13c59ac715ef6040dfa9d3d0815f659ceb663a41
Vegz78 <49032025+Vegz78@users.noreply.github.com>
2023-01-22 22:03:04 +0100

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image3":
+            case "horse0":return img`
+111111111111111111111111bc1111
+1111111111111111111111bcccc111
+1111111111111111111111cccccc11
+11111111111111111111cccccccc11
+111111111cccccb1111cccccccccc1
+1111111bccccccccccccccccc1bccc
+11111ccccccccccccccccccc111bbc
+111bccbcccccccccccccccc1111111
+ccccc111ccccccccccccccb1111111
+1ccc1111bcccccccccccccc1111111
+111111111bccc11ccccccccc111111
+1111111111ccc1111cccccccc11111
+1111111111bccc11111ccc11bc1111
+1111111111cbcc111111cc111cb111
+1111111111b1bcc111ccc1111bcb11
+11111111111111cc11bc1111111111
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
